5dc02119a271981a6f4532f1608e6e240d6c2053
Tomas Breuckmann <tomas.yoga@yahoo.com>
2022-01-13 18:40:40 -0300

    projeto 100%

--- script.js
+++ script.js
@@ -35,53 +35,66 @@ function testaConfirmacao() {
 testaConfirmacao();
 
 // criterio 21
-// Deve haver um campo com o formato "Nome: Alguem Aqui"
-// Deve haver um campo com o formato "Email: email@mail.com"
-// Deve haver um campo com o formato "Casa: Casa Escolhida"
-// Deve haver um campo com o formato "Família: Família Escolhida"
-// Deve haver um campo com o formato "Matérias: Matérias, Marcadas, Aqui"
-// Deve haver um campo com o formato "Avaliação: NotaAqui"
-// Deve haver um campo com o formato "Observações: Observações aqui"
-function mostraFormulario(event) {
-  event.preventDefault();
-  const evaluationForm = document.getElementById('evaluation-form');
-  const campo1 = ['Nome: ', 'Email: ', 'Casa: ', 'Família: ', 'Matérias: ', 'Avaliação: '];
-  campo1[6] = 'Observações: ';
+function selecionaMateria() {
+  const fMaterias = document.getElementsByClassName('selected');
+  let aux = 'Matérias: ';
+  for (let i = 0; i < fMaterias.length; i += 1) {
+    if (i === 0) {
+      aux += fMaterias[i].value;
+    } else {
+      aux = `${aux}, ${fMaterias[i].value}`;
+    }
+  }
+  return aux;
+}
+
+function selecionaAvaliacao() {
+  const fAvaliacao = document.getElementsByName('rate'); // para resgatar o que foi escolhido, percorrer o array e resgatar com o .value aquele que estiver com .checket == true
+  let aux = 'Avaliação: ';
+  for (let i = 0; i < fAvaliacao.length; i += 1) {
+    if (fAvaliacao[i].checked === true) {
+      aux += `${fAvaliacao[i].value}`;
+    }
+  }
+  return aux;
+}
+
+function selecionaFamilia() {
+  const fFamilia = document.getElementsByName('family'); // para resgatar o que foi escolhido, percorrer o array e resgatar com o .value aquele que estiver com .checked == true
+  let aux = 'Família: ';
+  for (let i = 0; i < fFamilia.length; i += 1) {
+    if (fFamilia[i].checked === true) {
+      aux += `${fFamilia[i].value}`;
+    }
+  }
+  return aux;
+}
+
+function coletaDados() {
   const campo2 = [];
   const fName = document.getElementById('input-name'); // .value para resgatar o input
   const fLastName = document.getElementById('input-lastname'); // .value para resgatar o input
-  campo2[0] = `${fName.value} ${fLastName.value}`;
+  campo2[0] = `Nome: ${fName.value} ${fLastName.value}`;
   const fEmail = document.getElementById('input-email'); // .value para resgatar o input
-  campo2[1] = fEmail.value;
+  campo2[1] = `Email: ${fEmail.value}`;
   const fCasa = document.getElementById('house'); // .value para resgatar o que foi escolhido
-  campo2[2] = fCasa.value;
-  const fFamilia = document.getElementsByName('family'); // para resgatar o que foi escolhido, percorrer o array e resgatar com o .value aquele que estiver com .checked == true
-  for (let i = 0; i < fFamilia.length; i += 1) {
-    if (fFamilia[i].checked === true) {
-      campo2[3] = fFamilia[i].value;
-    }
-  }
-  const fMaterias = document.getElementsByClassName('selected');// para resgatar o que foi escolhido (.subject), percorrer o array e resgatar com o .value
-  campo2[4] = '';
-  for (let i = 0; i < fMaterias.length; i += 1) {
-    if (i === 0) {
-      campo2[4] += fMaterias[i].value;
-    } else {
-      campo2[4] = `${campo2[4]}, ${fMaterias[i].value}`;
-    }
-  }
-  const fAvaliacao = document.getElementsByName('rate'); // para resgatar o que foi escolhido, percorrer o array e resgatar com o .value aquele que estiver com .checket == true
-  for (let i = 0; i < fAvaliacao.length; i += 1) {
-    if (fAvaliacao[i].checked === true) {
-      campo2[5] = fAvaliacao[i].value;
-    }
-  }
+  campo2[2] = `Casa: ${fCasa.value}`;
+  campo2[3] = selecionaFamilia();
+  campo2[4] = selecionaMateria();
+  campo2[5] = selecionaAvaliacao();
   const fObservacoes = document.getElementById('textarea');
-  campo2[6] = fObservacoes.value;
+  campo2[6] = `Observações: ${fObservacoes.value}`;
+  return campo2;
+}
+
+function mostraFormulario(event) {
+  event.preventDefault();
+  const evaluationForm = document.getElementById('evaluation-form');
+  const campo2 = coletaDados();
   evaluationForm.innerHTML = '';
-  for (let i = 0; i < campo1.length; i += 1) {
+  for (let i = 0; i < campo2.length; i += 1) {
     const campo = document.createElement('div');
-    campo.innerText = `${campo1[i]} ${campo2[i]}`;
+    campo.innerText = `${campo2[i]}`;
     evaluationForm.appendChild(campo);
   }
 }
